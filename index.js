292cc8e2a05df29bb7754daf00210a12f9fcfb5d
umeshmehta14 <ishaanmehta782@gmail.com>
2022-09-26 14:55:55 +0530

    completed with css

---
+++ index.js
@@ -0,0 +1,31 @@
+var btntranslate = document.querySelector("#btn-translate");
+
+var txtI = document.querySelector("#tta");
+var outputDiv = document.querySelector("#output");
+
+var serurl = "https://api.funtranslations.com/translate/valspeak.json"
+
+function gettranslate(text){
+    return serurl + "?" + "text=" + text;
+}
+
+function erroHandler(error) {
+    console.log("error occurred", error);
+    alert("something wrong with server");
+    
+}
+function clickHandler(){
+    // outputDiv.innerText = "wefreggreggv:" + txtI.value
+    var Itxt = txtI.value;
+
+    // calling server for processing
+    fetch(gettranslate(Itxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        // console.log(json.contents.translated)
+    })
+    .catch(erroHandler);
+}
+btntranslate.addEventListener("click", clickHandler)
